814bb5fcac4855c9750a6515acc31a593160e27d
sorai910 <107456617+sorai910@users.noreply.github.com>
2024-04-02 23:17:26 -0400

    docker

---
+++ assignment_3/streamlit_app/ner.py
@@ -0,0 +1,46 @@
+import io
+from spacy_streamlit import visualize_ner
+import pandas as pd
+import spacy
+from spacy import displacy
+nlp = spacy.load("en_core_web_sm")
+
+class SpacyDocument:
+
+    def __init__(self, text: str):
+        self.text = text
+        self.doc = nlp(text)
+
+    def get_tokens(self) -> list:
+        return [token.lemma_ for token in self.doc]
+
+    def get_entities(self) -> str:
+        entities = [(e.start_char, e.end_char, e.label_, e.text) for e in self.doc.ents]
+        return entities
+    
+    def get_dependency(self)-> str:
+        dependencies = [(token.text, token.dep_, token.head.text) for token in self.doc]
+        return dependencies
+
+    def get_markup_html(self) -> str:
+        html = displacy.render(self.doc, style="ent", page=True)+ "<br>"
+        html += pd.DataFrame(self.get_dependency(), columns=['Text', 'Dependency', 'Head Text']).to_html(index=False)
+        return html
+    
+    def get_ents_graph(self):
+        return visualize_ner(self.doc, labels = nlp.get_pipe("ner").labels)
+if __name__ == '__main__':
+
+    example = (
+        "When Sebastian Thrun started working on self-driving cars at "
+        "Google in 2007, few people outside of the company took him "
+        "seriously. “I can tell you very senior CEOs of major American "
+        "car companies would shake my hand and turn away because I wasn’t "
+        "worth talking to,” said Thrun, in an interview with Recode earlier "
+        "this week.")
+
+    doc = SpacyDocument(example)
+    print(doc.get_tokens())
+    for entity in doc.get_entities():
+        print(entity)
+    print(doc.get_entities_with_markup())
